f4616d230b27a25925e7d5962b4c525c4f065ffb
Leif Haberecht <81562562+Deskilling@users.noreply.github.com>
2026-05-24 02:29:44 +0200

    add flake and direnv to gitignore

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "gopkg";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            gnumake
+            go
+            gopls
+          ];
+        };
+      }
+    );
+}
